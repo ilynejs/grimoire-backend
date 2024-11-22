73cb88ed1b2a0ada4a7332871dea870736fd5361
Ilyne <ilynejs@proton.me>
2024-11-22 14:44:56 +0100

    Réorganisation et ajout de la logique à index.js

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-const app = require('express').express();
-const authRoutes = require('./routes/auth');
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./db');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require('./routes/auth.routes');
+// const bookRoutes = require('./routes/book');
 
 app.use('/api/auth', authRoutes);
+// app.use('/api/books', bookRoutes);
+
+connectDB();
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
