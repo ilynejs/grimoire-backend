c4434afe5d73ba1f96c6f8a45b10631302432023
Ilyne <ilynejs@proton.me>
2024-11-22 14:37:28 +0100

    Correction passage de put à post

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const userController = require('../controllers/user');
-
-router.put('/signup', userController.signup);
-router.post('/login', userController.login);
-
-module.exports = router;
