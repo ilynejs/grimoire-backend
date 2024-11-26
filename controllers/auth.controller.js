6d6963c295c5e89b7415c67e56906d98f309b81b
Ilyne <ilynejs@proton.me>
2024-11-26 21:33:54 +0100

    Vérification regex de la validité de l'adresse mail

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,6 +5,12 @@ require('dotenv').config();
 
 const signup = async (req, res, next) => {
 	try {
+		// Vérification avec regex s'il s'agit d'une adresse email
+		const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+		if (!emailRegex.test(req.body.email)) {
+			return res.status(400).json(new Error('Email is not valid'));
+		}
+
 		const hash = await bcrypt.hash(req.body.password, 10);
 		const user = new User({
 			email: req.body.email,
@@ -27,13 +33,13 @@ const login = async (req, res, next) => {
 		});
 
 		if (!user) {
-			return res.status(401).json({ message: 'Auth failed' });
+			return res.status(401).json(new Error('Auth failed'));
 		}
 
 		const result = await bcrypt.compare(req.body.password, user.password);
 
 		if (!result) {
-			return res.status(401).json({ message: 'Auth failed' });
+			return res.status(401).json(new Error('Auth failed'));
 		}
 
 		const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
