3d8d361025b357c06e3b0c47fa8d5690beacc053
Ilyne <ilynejs@proton.me>
2024-11-26 13:16:54 +0100

    Ajout de l'authentification

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
-	email: { type: String, required: true, unique: true },
+	email: { type: String, required: true, unique: true, trim: true },
 	password: { type: String, required: true },
 });
 
