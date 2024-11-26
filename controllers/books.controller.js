498eca59b5b6939a47ad779c90149fa98f3655b8
Ilyne <ilynejs@proton.me>
2024-11-26 21:28:44 +0100

    Fonctionnalités ajoutées : - Modification d'un livre - Ajout d'une note sur un livre.
    Fix : Gestion des erreurs et de l'authentification.

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -6,7 +6,7 @@ const getBooks = async (req, res, next) => {
 		const books = await Book.find();
 		res.json(books);
 	} catch (error) {
-		next(error);
+		res.status(400).json(error);
 	}
 };
 
@@ -14,11 +14,11 @@ const getOneBook = async (req, res, next) => {
 	try {
 		const book = await Book.findById(req.params.id);
 		if (!book) {
-			return res.status(404).json({ message: 'Book not found' });
+			return res.status(404).json(new Error('Book not found'));
 		}
 		res.json(book);
 	} catch (error) {
-		next(error);
+		res.status(400).json(error);
 	}
 };
 
@@ -27,7 +27,7 @@ const getBestRatingBooks = async (req, res, next) => {
 		const books = await Book.find().sort({ averageRating: -1 }).limit(3);
 		res.json(books);
 	} catch (error) {
-		res.status(400).json({ error: error.message });
+		res.status(400).json(error);
 	}
 };
 
@@ -54,26 +54,103 @@ const addBook = async (req, res, next) => {
 
 		await book.save();
 		res.status(201).json(book);
-
-		next();
 	} catch (error) {
-		res.status(400).json({ message: 'Invalid data' });
+		res.status(400).json(error);
 	}
 };
 
-const updateBook = async (req, res, next) => {};
+const updateBook = async (req, res, next) => {
+	try {
+		const book = await Book.findById(req.params.id);
+		if (!book) {
+			return res.status(404).json(new Error('Book not found'));
+		}
 
-const rateBook = async (req, res, next) => {};
+		if (book.userId.toString() !== req.auth.userId) {
+			return res.status(403).json({ message: 'Unauthorized request' });
+		}
+
+		console.log(req.body, req.file, req.body.book);
+		const bookObject = req.body.book
+			? JSON.parse(req.body.book)
+			: { ...req.body };
+
+		if (req.file && book.imageUrl) {
+			const filename = book.imageUrl.split('/images/')[1];
+			fs.unlink(`images/${filename}`, (err) => {
+				if (err) console.error(err);
+			});
+		}
+
+		const updatedData = {
+			...bookObject,
+			imageUrl: req.file
+				? `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+				: book.imageUrl,
+		};
+
+		const updatedBook = await Book.findOneAndUpdate(
+			{ _id: req.params.id, userId: req.auth.userId },
+			updatedData,
+			{ new: true },
+		);
+
+		if (!updatedBook) {
+			return res.status(404).json(new Error('Book not found'));
+		}
+
+		res.json(updatedBook);
+	} catch (error) {
+		res.status(400).json(error);
+	}
+};
+
+const rateBook = async (req, res, next) => {
+	try {
+		console.log(req.body, req.params.id);
+
+		const book = await Book.findById(req.params.id);
+		if (!book) {
+			return res.status(404).json(new Error('Book not found'));
+		}
+
+		const rating = book.ratings.find(
+			(rating) => rating.userId.toString() === req.auth.userId,
+		);
+		if (rating) {
+			rating.grade = req.body.rating;
+		} else {
+			book.ratings.push({
+				userId: req.auth.userId,
+				grade: req.body.rating,
+			});
+		}
+
+		const totalRating = book.ratings.reduce(
+			(acc, rating) => acc + rating.grade,
+			0,
+		);
+
+		book.averageRating = totalRating / book.ratings.length;
+		await book.save();
+
+		res.json(book);
+	} catch (error) {
+		res.status(400).json(error);
+	}
+
+	next();
+};
 
 const deleteBook = async (req, res, next) => {
 	try {
 		const book = await Book.findById(req.params.id);
 		if (!book) {
-			return res.status(404).json({ message: 'Book not found' });
+			return res.status(404).json(new Error('Book not found'));
 		}
 
 		if (book.userId.toString() !== req.auth.userId) {
-			return res.status(403).json({ message: 'Unauthorized' });
+			return res.status(403).json(new Error('Unauthorized request'));
 		}
 
 		if (book.imageUrl) {
@@ -85,8 +162,10 @@ const deleteBook = async (req, res, next) => {
 
 		res.status(204).end();
 	} catch (error) {
-		res.status(400).json({ error: error.message });
+		res.status(400).json(error);
 	}
+
+	next();
 };
 
 module.exports = {
